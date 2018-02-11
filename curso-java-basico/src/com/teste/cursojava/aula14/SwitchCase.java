5a329050332030c24c9010350c1cbcf5104ba48a
Gustavo Clay <gustavoclay@gmail.com>
2018-02-11 16:28:40 -0200

    Switch Case, Revisão Java

---
+++ curso-java-basico/src/com/teste/cursojava/aula14/SwitchCase.java
@@ -0,0 +1,42 @@
+package com.teste.cursojava.aula14;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("dia da semana(1-7): ");
+		int diaSemana = scan.nextInt();
+
+		switch (diaSemana) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda");
+			break;
+		case 3:
+			System.out.println("Terça");
+			break;
+		case 4:
+			System.out.println("Quarta");
+			break;
+		case 5:
+			System.out.println("Quinta");
+			break;
+		case 6:
+			System.out.println("Sexta");
+			break;
+		case 7:
+			System.out.println("Sabado");
+			break;
+		default:
+			System.out.println("Não é um dia válido");
+			break;
+		}
+
+	}
+
+}
