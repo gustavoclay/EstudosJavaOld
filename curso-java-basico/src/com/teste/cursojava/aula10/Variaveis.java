28303aeb25013d4b78974300f84ac71e53a754d9
Gustavo Clay <gustavoclay@gmail.com>
2018-02-11 11:34:25 -0200

    Exercicio Variaveis, revisando conteúdo e realizando testes

---
+++ curso-java-basico/src/com/teste/cursojava/aula10/Variaveis.java
@@ -0,0 +1,18 @@
+package com.teste.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		int idade = 18;
+		String nome = "Gustavo";
+		String nomeDoCachoro = "Totó";
+		String ano2014 = "2014";
+
+		System.out.println(idade);
+		System.out.println(nome);
+		System.out.println(nomeDoCachoro);
+		System.out.println(ano2014);
+
+	}
+
+}
