74b4e58ce38328dc905495dc5b979cef09e3d512
Gustavo Clay <gustavoclay@gmail.com>
2018-02-13 14:00:25 -0200

    OO, Construtores, Revisão Java

---
+++ curso-java-basico/src/com/teste/cursojava/aula29/Carro.java
@@ -0,0 +1,42 @@
+package com.teste.cursojava.aula29;
+
+public class Carro {
+
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	
+	
+	Carro(){
+		System.out.println("Classe carro foi instanciada!");
+		numPassageiros = 4;
+	}
+	
+	Carro(String marca_, String modelo_, int numPassageiros_, double capCombustivel_, double consumoCombustivel_){
+		marca = marca_;
+		modelo = modelo_;
+		numPassageiros = numPassageiros_;
+		capCombustivel = capCombustivel_;
+		consumoCombustivel = consumoCombustivel_;
+	}
+
+	void exibirAutonomia() {
+		System.out.println("A autonomia do Carro é: " + capCombustivel * consumoCombustivel + " KM.");
+	}
+
+	double obterAutonomia() {
+
+		System.out.println("Método obterAutonomia foi chamado!");
+
+		return capCombustivel * consumoCombustivel;
+	}
+
+	double calcularCombustivel(double km) {
+		double qtdCombustivel = km / consumoCombustivel;
+		return qtdCombustivel;
+	}
+
+}
---
+++ curso-java-basico/src/com/teste/cursojava/aula29/Carro2.java
@@ -0,0 +1,26 @@
+package com.teste.cursojava.aula29;
+
+public class Carro2 {
+
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	public Carro2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Carro2(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numPassageiros = numPassageiros;
+		this.capCombustivel = capCombustivel;
+		this.consumoCombustivel = consumoCombustivel;
+	}
+	
+	
+}
