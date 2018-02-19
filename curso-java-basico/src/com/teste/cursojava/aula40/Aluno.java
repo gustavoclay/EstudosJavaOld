8e3b104424b44b5754cf3bd095fab5af4f035d0b
Gustavo Clay <gustavoclay@gmail.com>
2018-02-19 11:42:39 -0300

    OO, Polimormismo em tempo de execução, Revisão Java

---
+++ curso-java-basico/src/com/teste/cursojava/aula40/Aluno.java
@@ -0,0 +1,48 @@
+package com.teste.cursojava.aula40;
+
+public class Aluno extends Pessoa {
+
+	private String[] cursos;
+	private String[][] notas;
+
+	public Aluno(String nome, String endereco, String telefone, String[] curso) {
+		super(nome, endereco, telefone);
+		this.cursos = curso;
+	}
+
+	public Aluno() {
+		super();
+	}
+
+	public String[] getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(String[] cursos) {
+		this.cursos = cursos;
+	}
+
+	public String[][] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(String[][] notas) {
+		this.notas = notas;
+	}
+
+	public double calculaMedia() {
+		return 0;
+	}
+
+	public boolean verificarAprovado() {
+		return true;
+	}
+
+	public String obterEtiquetaEndereco() {
+		String s = "Endereço do Aluno: ";
+		s += this.getEndereco();
+
+		return s;
+	}
+
+}
